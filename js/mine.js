46bbdde8474f31457c087008aecbbd344b1026eb
Sree Hari Krishna <sreeharikrishna.a@RACKSPACE.COM>
2015-03-24 01:04:00 +0530

    Initial Version

---
+++ js/mine.js
@@ -0,0 +1,11 @@
+﻿function updateMap(lat, long){
+    //map = new GMaps({
+    //    el: '#map',
+    //    zoom: 12,
+    //    lat: 11.071777, 
+    //    lng: 77.001631,
+    //    //lat: '<%=latitude%>',
+    //    //lng: '<%=longitude%>'
+    //});
+    map.setCenter(11.071777, 77.001631);
+}

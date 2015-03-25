5ede912668a302ce4a559c5834d3e49dc914a80b
Sree Hari Krishna <sreeharikrishna.a@RACKSPACE.COM>
2015-03-25 12:32:45 +0530

    Added DB project

---
+++ DAL/DroidhashGPS/Role.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Role]
+(
+	[RoleId] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL
+)
---
+++ DAL/DroidhashGPS/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserName] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [RoleId] INT NOT NULL, 
+    CONSTRAINT [FK_User_ToTable] FOREIGN KEY (RoleId) REFERENCES Role(RoleId) 
+)
